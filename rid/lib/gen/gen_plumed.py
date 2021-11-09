1c548e28091fa6f395332c95cfb50e9f4ed3d421
Dead-fisher <1800011721@pku.edu.cn>
2021-11-09 23:31:51 +0800

    modify the format

--- rid/lib/gen/gen_plumed.py
+++ rid/lib/gen/gen_plumed.py
@@ -84,6 +84,64 @@ def user_plumed(cv_file, pstride, pfile):
     return ret, cv_names, print_content
 
 
+def json2plumed(CONF, CV_FILE):
+    
+    residues, residue_atoms = make_ndx(CONF)
+    protein_atom_idxes = make_protein_atom_index(CONF)
+    with open(CV_FILE, 'r') as fp:
+        jdata = json.load(fp)
+    dih_angles = jdata["dih_angles"]
+    fmt_alpha = jdata["alpha_idx_fmt"]
+    fmt_angle = jdata["angle_idx_fmt"]
+    hp_residues = []
+    dist_atom = []
+    dist_excl = 10000
+    if "hp_residues" in jdata:
+        hp_residues = jdata["hp_residues"]
+    if "dist_atom" in jdata:
+        dist_atom = jdata["dist_atom"]
+    if "dist_excl" in jdata:
+        dist_excl = jdata["dist_excl"]
+
+    angle_names, angle_atom_idxes = make_general_angle_def(
+        residue_atoms, dih_angles, fmt_alpha, fmt_angle)
+
+    dist_names, dist_atom_idxes = make_general_dist_def(
+        residues, residue_atoms, hp_residues, dist_atom, fmt_alpha, dist_excl)
+
+    if "selected_index" in jdata:
+        selected_index = jdata["selected_index"]
+        selected_angle_index = []
+        for ssi in selected_index:
+            if ssi == 0:
+                selected_angle_index.append(0)
+            elif ssi != 0:
+                selected_angle_index.append(ssi*2-1)
+                selected_angle_index.append(ssi*2)
+
+        newselected_angle_index = [
+            i for i in selected_angle_index if i < len(angle_names)]
+        new_angle_names = [angle_names[i] for i in newselected_angle_index]
+        new_angle_atom_idxes = [angle_atom_idxes[i]
+                                for i in newselected_angle_index]
+    else:
+        new_angle_names = angle_names
+        new_angle_atom_idxes = angle_atom_idxes
+
+    ret = ""
+
+    if len(dist_names) > 0:
+        ret += make_wholemolecules(protein_atom_idxes)
+        ret += "\n"
+    ret += (make_angle_def(new_angle_names, new_angle_atom_idxes))
+    ret += (make_dist_def(dist_names, dist_atom_idxes))
+    ret += "\n"
+
+    cv_names = new_angle_names + dist_names
+    print_content = None
+    return ret, cv_names, print_content
+
+
 def general_plumed(TASK,
                    CONF,
                    CV_FILE,
@@ -93,61 +151,9 @@ def general_plumed(TASK,
                    gamma=0.1,
                    pstride=5,
                    pfile="plm.out"):
+                   
     if CV_FILE.split(".")[-1] == "json":
-        with open(CV_FILE, 'r') as fp:
-            jdata = json.load(fp)
-        residues, residue_atoms = make_ndx(CONF)
-        protein_atom_idxes = make_protein_atom_index(CONF)
-        dih_angles = jdata["dih_angles"]
-        fmt_alpha = jdata["alpha_idx_fmt"]
-        fmt_angle = jdata["angle_idx_fmt"]
-        hp_residues = []
-        dist_atom = []
-        dist_excl = 10000
-        if "hp_residues" in jdata:
-            hp_residues = jdata["hp_residues"]
-        if "dist_atom" in jdata:
-            dist_atom = jdata["dist_atom"]
-        if "dist_excl" in jdata:
-            dist_excl = jdata["dist_excl"]
-
-        angle_names, angle_atom_idxes = make_general_angle_def(
-            residue_atoms, dih_angles, fmt_alpha, fmt_angle)
-
-        dist_names, dist_atom_idxes = make_general_dist_def(
-            residues, residue_atoms, hp_residues, dist_atom, fmt_alpha, dist_excl)
-
-        if "selected_index" in jdata:
-            selected_index = jdata["selected_index"]
-            selected_angle_index = []
-            for ssi in selected_index:
-                if ssi == 0:
-                    selected_angle_index.append(0)
-                elif ssi != 0:
-                    selected_angle_index.append(ssi*2-1)
-                    selected_angle_index.append(ssi*2)
-
-            newselected_angle_index = [
-                i for i in selected_angle_index if i < len(angle_names)]
-            new_angle_names = [angle_names[i] for i in newselected_angle_index]
-            new_angle_atom_idxes = [angle_atom_idxes[i]
-                                    for i in newselected_angle_index]
-        else:
-            new_angle_names = angle_names
-            new_angle_atom_idxes = angle_atom_idxes
-
-        ret = ""
-
-        if len(dist_names) > 0:
-            ret += make_wholemolecules(protein_atom_idxes)
-            ret += "\n"
-        ret += (make_angle_def(new_angle_names, new_angle_atom_idxes))
-        ret += (make_dist_def(dist_names, dist_atom_idxes))
-        ret += "\n"
-
-        cv_names = new_angle_names + dist_names
-        print_content = None
-
+        ret, cv_names, print_content = json2plumed(CONF, CV_FILE)
     else:
         ret, cv_names, print_content = user_plumed(CV_FILE, pstride, pfile)
 

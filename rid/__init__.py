1110e8d57a6e64e6a2cd1342905f5a825c98d5af
yanze <1800011721@pku.edu.cn>
2022-06-26 15:14:40 +0800

    delet

--- rid/__init__.py
+++
@@ -1,5 +0,0 @@
-from rid.gen_rid import gen_rid
-import sys
-ROOT_PATH = __path__[0]
-NAME = "ridkit"
-sys.path.append(ROOT_PATH)
--- rid/lib/__init__.py
+++
@@ -1,3 +0,0 @@
-import sys
-LIB_PATH = __path__[0]
-sys.path.append(LIB_PATH)

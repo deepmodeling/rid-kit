61ee057f48c668dcf09854a7146de0baa02f5bff
yanze <yanze039@mit.edu>
2022-07-03 12:56:39 +0800

    change name

--- rid/op/prep_select.py
+++ rid/op/prep_select.py
@@ -9,7 +9,12 @@ from typing import Tuple, List, Optional, Dict, Union
 from pathlib import Path
 from rid.select.cluster import Cluster
 from rid.utils import save_txt
-from rid.constants import culster_selection_data_name, culster_selection_index_name
+from rid.constants import (
+    culster_selection_data_name, 
+    culster_selection_index_name, 
+    cls_sel_precision, 
+    cls_ndx_precision
+)
 import numpy as np
 
 
@@ -49,11 +54,11 @@ class PrepSelector(OP):
         cv_cluster = Cluster(
             data, op_in["cluster_threshold"], angular_mask=op_in["angular_mask"], 
             weights=op_in["weights"], max_selection=op_in["max_selection"])
-        threshold = cv_cluster.make_threshold(op_in["numb_cluster_upper"], op_in["numb_cluster_lower"])
+        threshold = cv_cluster.make_threshold(op_in["numb_cluster_lower"], op_in["numb_cluster_upper"])
         cls_sel = cv_cluster.get_cluster_selection()
-        save_txt(culster_selection_index_name, cls_sel)
+        save_txt(culster_selection_index_name, cls_sel, fmt=cls_ndx_precision)
         selected_data = data[cls_sel]
-        save_txt(culster_selection_data_name, selected_data)
+        save_txt(culster_selection_data_name, selected_data, fmt=cls_sel_precision)
         op_out = OPIO({
                 "cluster_threshold": threshold,
                 "culster_selection_index": Path(culster_selection_index_name),

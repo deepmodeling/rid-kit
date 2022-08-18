ec3545f23abbf2ee1d190d6704f115c3cfd36369
PKUfjh <2001110077@pku.edu.cn>
2022-08-18 14:44:18 +0800

    add source code and index to docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,6 +40,7 @@ extensions = [
     'sphinx.ext.autosummary',
     'myst_parser',
     'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
     'nbsphinx'
 ]
 napoleon_numpy_docstring = True
@@ -66,6 +67,7 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+master_doc = 'index'
 
 def run_apidoc(_):
     from sphinx.ext.apidoc import main

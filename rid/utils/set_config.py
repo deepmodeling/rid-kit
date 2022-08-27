dc5ef6e5a47eae35fcbf3ace2b76c886febfe9c5
Dead-fisher <yanze039@mit.edy>
2022-08-27 12:13:48 +0800

    update submission configs

--- rid/utils/set_config.py
+++ rid/utils/set_config.py
@@ -1,6 +1,6 @@
 from typing import Dict
 from dflow.plugins.lebesgue import LebesgueExecutor
-from dflow import SlurmRemoteExecutor
+from dflow.plugins.dispatcher import DispatcherExecutor
 
 
 def init_executor(
@@ -8,11 +8,14 @@ def init_executor(
     ):
     if executor_dict is None:
         return None
-    etype = executor_dict.pop('type')
+    if "type" in executor_dict:
+        etype = executor_dict.pop('type')
+    else:
+        etype = executor_dict["machine_dict"]["batch_type"]
     if etype.lower() == "lebesgue_v2":
         return LebesgueExecutor(**executor_dict)
     elif etype.lower() == "slurm":
-        return SlurmRemoteExecutor(**executor_dict)
+        return DispatcherExecutor(**executor_dict)
     else:
         raise RuntimeError('unknown executor type', etype)    
 
@@ -24,7 +27,9 @@ def normalize_resources(config_dict: Dict):
     if template_dict["executor"] is None:
         assert ("image" in template_dict["template_config"].keys()) and \
             template_dict["template_config"]["image"] is not None
-    if template_dict["executor"] is not None and template_dict["executor"]["type"] == "slurm":
+    elif template_dict["executor"] is not None and not "type" in template_dict["executor"]:
+        return template_dict
+    elif template_dict["executor"] is not None and template_dict["executor"]["type"] == "slurm":
         header_list = template_dict["executor"]["header"]
         template_dict["executor"]["header"] = "\n".join(header_list)
     return template_dict
